b1f2bbca5f8eb3f9175fe129e3306795681f3afb
ahanley96 <a.hanley96@hotmail.co.uk>
2020-03-20 23:21:43 +0000

    Started API and called on frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from './services/hackernewsAPI'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const App = () => {
+
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data))
+  }, []);
+
+  return <p1>{JSON.stringify(storyIds)}</p1>;
+
 }
-
-export default App;
---
+++ src/services/hackernewsAPI.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const newsStoriesUrl = `${baseUrl}newstories.json`;
+export const storyUrl = `${baseUrl}item/`;
+
+export const getStoryIds = async () => {
+
+    const result = axios.get(newsStoriesUrl).then(data => data)
+
+    return result; 
+
+}
