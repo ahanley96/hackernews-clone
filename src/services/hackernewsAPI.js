9b4a06bf185f90f056308c430e70b5498bda924d
ahanley96 <a.hanley96@hotmail.co.uk>
2020-03-28 12:43:35 +0000

    Tidy up

--- src/services/hackernewsAPI.js
+++ src/services/hackernewsAPI.js
@@ -6,7 +6,7 @@ export const topStoriesUrl = `${baseUrl}topstories.json`;
 export const bestStoriesUrl = `${baseUrl}beststories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
-
+//Will be used for quering individual stories
 export const getStory = async (storyId) => {
     
     const result = await axios
@@ -17,26 +17,35 @@ export const getStory = async (storyId) => {
 
 }
 
+//Queries the "/newstories" hackernews API for top 500 id's
 export const getNewStoryIds = async () => {
 
-    const result = await axios.get(newStoriesUrl)
+    const result = await axios
+    
+    .get(newStoriesUrl)
 
     return result.data
 
 }
 
+//Queries the "/topstories" hackernews API for top 500 id's
 export const getTopStoryIds = async () => {
 
-    const result = axios.get(topStoriesUrl).then(data => data)
+    const result = await axios
+    
+    .get(topStoriesUrl).then(data => data)
 
-    return result; 
+    return result.data
 
 }
 
+//Queries the "/beststories" hackernews API for top 500 id's
 export const getBestStoryIds = async () => {
 
-    const result = axios.get(bestStoriesUrl).then(data => data)
+    const result = await axios
+    
+    .get(bestStoriesUrl).then(data => data)
 
-    return result; 
+    return result.data; 
 
 }
